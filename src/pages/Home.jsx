1e1980b44cbdf7c5ce0f7267905a168a41cef95a
Odilbek798 <odilbekiskandaroviskandarov@gmail.com>
2024-11-26 16:09:04 +0500

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import Hero from '../components/Hero/Hero'
+import Category from '../components/Category/Category'
+import Browse from '../components/Browse/Browse'
+import Footer from '../components/Footer/Footer'
+
+const Home = () => {
+  return (
+    <>
+    <Header/>
+    <Hero/>
+    <Category/>
+    <Browse/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
